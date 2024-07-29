a63467e256f0c8118bedb4ca5939999a012857a4
rameez543 <rameez@getnuo.com>
2024-07-30 01:01:03 +0530

    added test case for add method

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -1,10 +1,28 @@
 const stringCalcMethods = require("./StringCalculator");
 
 const stringToNumberArray = stringCalcMethods.stringToNumberArray;
+const add = stringCalcMethods.add;
 
 test("string to number array test1", () => {
   expect(stringToNumberArray("123,dswe$-1")).toStrictEqual([123, -1]);
 });
 test("string to number array test2", () => {
   expect(stringToNumberArray("12&3,dswe$-1,2")).toStrictEqual([12, 3, -1, 2]);
+});
+test("string to number array test3", () => {
+  expect(stringToNumberArray("")).toStrictEqual([]);
+});
+test("string to number array test3", () => {
+  expect(stringToNumberArray("/nabcd@#@$@")).toStrictEqual([]);
+});
+test("string add test1", () => {
+  expect(add("123,dswe$-1/n-2")).toStrictEqual(
+    "negative numbers not allowed -1,-2"
+  );
+});
+test("string add test2", () => {
+  expect(add("")).toStrictEqual(0);
+});
+test("string add test3", () => {
+  expect(add("1ssdsdz%")).toStrictEqual(1);
 });
