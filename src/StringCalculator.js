84ffa22183c3dda31b81e81e0579370a3cb9416f
rameez543 <rameez@getnuo.com>
2024-07-30 01:00:50 +0530

    add method

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,6 +1,24 @@
 //this function accept a string and returns an array with numbers in the string
 function stringToNumberArray(str) {
-  return str.match(/-?\d+/g).map(Number);
+  const matches = str.match(/-?\d+/g);
+  return matches?.length > 0 ? matches.map(Number) : [];
 }
 
-module.exports = { stringToNumberArray: stringToNumberArray };
+function add(str) {
+  const numberArr = stringToNumberArray(str);
+  const negativeNumbers = [];
+  let sum = 0;
+  for (num of numberArr) {
+    if (num >= 0) {
+      sum = sum + num;
+    } else {
+      negativeNumbers.push(num);
+    }
+  }
+  if (negativeNumbers.length > 0) {
+    return `negative numbers not allowed ${negativeNumbers.join(",")}`;
+  }
+  return sum;
+}
+
+module.exports = { stringToNumberArray: stringToNumberArray, add: add };
